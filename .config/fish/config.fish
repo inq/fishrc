84b117bec378579481105dc93156a29259ba52d5
inkyu <gofiri@gmail.com>
2016-07-13 12:09:38 +0900

    initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+set PATH $PATH /usr/home/q/.cabal/bin/
+rvm default
+
+abbr -a pv pkg version | grep "<"
+abbr -a gp git pull
+abbr -a gf git fetch
+abbr -a gs git statsus
---
+++ README.md
@@ -0,0 +1,6 @@
+fishrc
+======
+
+the run commands for the fish
+
+inkyu lee, 2016
